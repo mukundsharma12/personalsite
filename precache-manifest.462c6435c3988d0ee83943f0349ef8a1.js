1462485b0048e6c5003be667eef2b5e37e1232a5
Mukund <mukundrssharma@gmail.com>
2022-07-21 18:27:21 +0530

    Updates

---
+++ precache-manifest.462c6435c3988d0ee83943f0349ef8a1.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fb57f9038ceea2d1723",
+    "url": "/personalsite/static/js/main.9fb57f90.chunk.js"
+  },
+  {
+    "revision": "55c357cafadfe3e7633a",
+    "url": "/personalsite/static/css/1.26878743.chunk.css"
+  },
+  {
+    "revision": "55c357cafadfe3e7633a",
+    "url": "/personalsite/static/js/1.55c357ca.chunk.js"
+  },
+  {
+    "revision": "b7dd6a8798293e0a5168",
+    "url": "/personalsite/static/js/runtime~main.b7dd6a87.js"
+  },
+  {
+    "revision": "fd5d71d5e48c519d2b62b7235033427d",
+    "url": "/personalsite/static/media/ms-logo.fd5d71d5.png"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/personalsite/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "0393a6c85ddc105c9304f0f7787ddaea",
+    "url": "/personalsite/static/media/nav-icon2.0393a6c8.svg"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/personalsite/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "dea05c56b45857a24dcba5a23202ec70",
+    "url": "/personalsite/static/media/meter1.dea05c56.svg"
+  },
+  {
+    "revision": "70af9240bb95dd2d65f6bf9f09591507",
+    "url": "/personalsite/static/media/project-img1.70af9240.png"
+  },
+  {
+    "revision": "98b3bc237950d6cb1a1e49f75372ae36",
+    "url": "/personalsite/static/media/project-img2.98b3bc23.png"
+  },
+  {
+    "revision": "08e5b04289e11fe1c0776cef35c4bcf2",
+    "url": "/personalsite/static/media/project-img3.08e5b042.png"
+  },
+  {
+    "revision": "bfe6d1a6d579d73c0c625ec0afe2855b",
+    "url": "/personalsite/static/media/header-img.bfe6d1a6.svg"
+  },
+  {
+    "revision": "7fb1b05f49ba5c3b4570e1dec3728213",
+    "url": "/personalsite/static/media/meter2.7fb1b05f.svg"
+  },
+  {
+    "revision": "19e4204daee7b06ccd0aa7807bc37653",
+    "url": "/personalsite/static/media/meter3.19e4204d.svg"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/personalsite/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/personalsite/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/personalsite/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/personalsite/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/personalsite/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/personalsite/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "82e0dc105bd444cfa124b7be0a8e3652",
+    "url": "/personalsite/index.html"
+  }
+];
